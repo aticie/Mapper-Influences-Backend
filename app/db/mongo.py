96aa1c4835767a0f6ba7f5f2a84ffb498dbe4a05
112batuhan <batuhancakir97@hotmail.com>
2024-05-14 22:26:28 +0300

    feat: single client for mongo

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -96,3 +96,21 @@ class AsyncMongoClient(AsyncIOMotorClient):
                 "country": 1
             }}
         ]).to_list(length=None)
+
+
+# singleton mongo client
+mongo_client: AsyncMongoClient = None
+
+
+async def start_mongo_client(mongo_url: str):
+    global mongo_client
+    mongo_client = AsyncMongoClient(mongo_url)
+
+
+async def close_mongo_client():
+    if mongo_client is not None:
+        mongo_client.close()
+
+
+async def get_mongo_db() -> AsyncIOMotorClient:
+    return mongo_client
