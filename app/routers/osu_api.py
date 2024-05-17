f906fb1989da5617c1fed43f8184898dc0b2365a
112batuhan <batuhancakir97@hotmail.com>
2024-05-17 03:54:20 +0300

    feat: added beatmapset endpoint

--- app/routers/osu_api.py
+++ app/routers/osu_api.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 import aiohttp
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.utils.jwt import decode_user_token
 from app.utils.encryption import decrypt_string
@@ -8,13 +8,22 @@ from app.utils.encryption import decrypt_string
 router = APIRouter(prefix="/osu_api", tags=["osu Api"])
 
 
-@router.get("/beatmap/{beatmap_id}", summary="get beatmap data using osu api")
-async def get_beatmap(
-    beatmap_id: int,
+@router.get("/beatmap/{id}", summary="get beatmapset data using osu api. Use type=beatmap to get beatmap data")
+async def get_beatmapset(
+    id: int,
     user: Annotated[dict, Depends(decode_user_token)],
+    type: str | None = None,
 ):
     access_token = decrypt_string(user["access_token"])
-    return await get_beatmap(access_token, beatmap_id)
+
+    if type == "beatmapset" or type is None:
+        return await get_beatmapset(access_token, id)
+    elif type == "beatmap":
+        beatmap = await get_beatmap(access_token, id)
+        return await get_beatmapset(access_token, beatmap["beatmapset_id"])
+    else:
+        raise HTTPException(
+            status_code=400, detail="Invalid type, type can be 'beatmap' or 'beatmapset'")
 
 
 @router.get("/user/{user_id}", summary="get user data using osu api")
@@ -51,6 +60,14 @@ async def get_beatmap(access_token: str, beatmap_id: int):
     auth_header = {"Authorization": f"Bearer {access_token}"}
     async with aiohttp.ClientSession(headers=auth_header) as session:
         async with session.get(beatmap_url) as response:
+            return await response.json()
+
+
+async def get_beatmapset(access_token: str, beatmapset_id: int):
+    beatmapset_url = f"https://osu.ppy.sh/api/v2/beatmapsets/{beatmapset_id}"
+    auth_header = {"Authorization": f"Bearer {access_token}"}
+    async with aiohttp.ClientSession(headers=auth_header) as session:
+        async with session.get(beatmapset_url) as response:
             return await response.json()
 
 
