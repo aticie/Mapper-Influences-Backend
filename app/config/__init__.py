e58564f4556241b5064917e0abd99b393f9e7262
112batuhan <batuhancakir97@hotmail.com>
2024-05-18 07:51:16 +0300

    chore: adding logout redirect uri to config

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -10,6 +10,7 @@ class APISettings(BaseSettings):
     OSU_CLIENT_SECRET: str
     OSU_REDIRECT_URI: str
     POST_LOGIN_REDIRECT_URI: str
+    POST_LOGOUT_REDIRECT_URI: str
 
 
 class AuthSettings(BaseSettings):
