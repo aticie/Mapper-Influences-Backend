1fcd2cac871930a1fe3d2dddaeafd20dab459418
112batuhan <36697363+112batuhan@users.noreply.github.com>
2024-05-20 02:25:55 +0300

    disable validation temporariliy

--- app/routers/leaderboard.py
+++ app/routers/leaderboard.py
@@ -5,14 +5,14 @@ from app.db.mongo import AsyncMongoClient, User, get_mongo_db
 router = APIRouter(prefix="/leaderboard", tags=["leaderboard"])
 
 
-@router.get("", response_model=list[User], summary="Get top users which are most mentioned by others")
+@router.get("",  summary="Get top users which are most mentioned by others")
 async def get_leaderboard(
     mongo_db: AsyncMongoClient = Depends(get_mongo_db),
 ):
     return await mongo_db.get_leaderboard()
 
 
-@router.get("/ranked", response_model=list[User], summary="Get top users which are mentioned by ranked mappers")
+@router.get("/ranked", summary="Get top users which are mentioned by ranked mappers")
 async def get_leaderboard(
     mongo_db: AsyncMongoClient = Depends(get_mongo_db),
 ):
