e6731204e4291d1823c1004603a18535ae88076e
112batuhan <batuhancakir97@hotmail.com>
2024-05-25 14:50:32 +0300

    feat: added sentry

--- app/main.py
+++ app/main.py
@@ -5,12 +5,26 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
+import sentry_sdk
+
 
 from app.db.instance import close_mongo_client, start_mongo_client
 from app.routers import auth, influence, osu_api_full_response, user, leaderboard, osu_api
 from app.config import settings
 
 logger = logging.getLogger(__name__)
+
+
+sentry_sdk.init(
+    dsn=settings.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
 
 
 @asynccontextmanager
