21d5c9d01a82900978c84da860e388852b550824
112batuhan <batuhancakir97@hotmail.com>
2024-06-01 20:45:33 +0300

    chore: more rebustation

--- app/test/test_leaderboard.py
+++ app/test/test_leaderboard.py
@@ -12,3 +12,5 @@ async def test_leaderboard(test_client, mongo_db, headers, test_user_id):
     assert response.status_code == 200
     response = await test_client.get("leaderboard?ranked=true&country=CN&limit=25&skip=0")
     assert response.status_code == 200
+    response = response.json()
+    assert len(response) >= 1
