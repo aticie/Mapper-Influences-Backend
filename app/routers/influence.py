7f927b257a3b1bd5d23b86693c8911fae2c10dd6
112batuhan <batuhancakir97@hotmail.com>
2024-06-06 00:56:36 +0300

    shameful print delete

--- app/routers/influence.py
+++ app/routers/influence.py
@@ -56,7 +56,6 @@ async def add_influence(
         description=influence_request.description,
         influenced_to=influence.influenced_to,
     )
-    print(user)
 
     await activity_ws.collect_acitivity(
         ActivityType.ADD_INFLUENCE, user_data=user, details=activity_details)
